4dea6760357948dedcccf4164155753172965ea5
HARI-3728 <htharn760@gmail.com>
2026-09-05 01:47:16 +0530

    change9

--- Backend/server.js
+++ Backend/server.js
@@ -57,7 +57,7 @@ const defaultOrigins = [
   "http://localhost:5174",
   "http://localhost:3000",
   "https://edunovaai.onrender.com",
-  "https://edunova-c6t3n9c63-athens2.vercel.app"
+  "https://edunova-fpa37b2tx-athens2.vercel.app"
 ];
 
 const allowedOrigins = [
